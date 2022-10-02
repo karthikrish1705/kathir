1074ca819e01197d8d78245ca64f7e9f0ae5560e
hp <hp@krish>
2022-10-02 08:42:24 +0530

    FIRSTCOMMIT

---
+++ kathirallure/src/main/java/basics/c1.java
@@ -0,0 +1,23 @@
+package basics;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+
+public class c1 {
+
+	@Description("Visiting gooogle")
+	@Feature("GOOGLE HOME")
+	@Severity(SeverityLevel.NORMAL)
+    @Test
+	public void h()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Desktop\\chromedriver.exe");
+		ChromeDriver ob= new ChromeDriver();
+		ob.get("http://www.google.com");
+	}
+}
